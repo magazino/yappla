8383e34f3ba6ea4a87dcea0e0f5f2f810c3f5352
Daniele Calisi <calisi@magazino.eu>
2023-04-03 12:08:33 +0200

    Better handling of versions

--- yappla/__init__.py
+++ yappla/__init__.py
@@ -9,4 +9,32 @@ from .domain import Domain
 from .planner import Planner
 
 __version__ = "0.0.1"
+try:
+    git_version = subprocess.check_output(
+        ["git", "describe", "--tags", "--dirty=-wip"], stderr=subprocess.STDOUT
+    )
+    output = git_version.strip().decode("ascii")
+    data = output.split("-")
+    tag = data[0]
+    match = re.match(r"^v(\d+)\.(\d)+\.(\d)$", tag)
+    if match is not None:
+        MAJOR, MINOR, REL = tuple(int(x) for x in match.groups())
+    import code; code.interact(local=locals())
+    try:
+        COMMITS = int(data[1])
+    except ValueError:
+        COMMITS = 0
+
+    if data[-1] == "wip":
+        if COMMITS == 0:
+            VERSION = (MAJOR, MINOR, REL, "post", 1)
+            __version__ = f"{MAJOR}.{MINOR}.{REL}.post1"
+        else:
+            VERSION = (MAJOR, MINOR, REL, COMMITS, "post", 1)
+            __version__ = f"{MAJOR}.{MINOR}.{REL}.{COMMITS}.post1"
+    else:
+        VERSION = (MAJOR, MINOR, REL, COMMITS, "dev", 1)
+        __version__ = f"{MAJOR}.{MINOR}.{REL}.{COMMITS}.dev1"
+except Exception as ex:
+    pass
 
